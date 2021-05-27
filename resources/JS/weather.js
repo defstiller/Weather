c58a1795513e0af4978794709d3988bfc22ceb99
defstiller <81063003+defstiller@users.noreply.github.com>
2021-05-27 12:03:25 -0500

    weather website, api added, html added, css, sign up has been adjusted to fit new needs

---
+++ resources/JS/weather.js
@@ -0,0 +1,64 @@
+var temp;  						// WEATHER API START 
+var weather;
+function getLocation() {
+	if (navigator.geolocation) {
+		let options = {timeout:50000, enableHighAccuracy:true, maximumAge: 200000}
+	  navigator.geolocation.getCurrentPosition(showLocation,error, options);
+	} else { 
+	  window.alert = "Geolocation is not supported by this browser.";
+	}
+  }
+  function error(err){
+	  if(err.code ==1 ) {
+		  console.log("Denied");
+	  }
+	  else if (err.code == 2 ){
+		  console.log("Position is unavailable")
+	  }
+  }
+  function showLocation(position) {
+	  var lat = position.coords.latitude;
+	  var lon = position.coords.longitude;
+	  fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=e5c0ad89eb574fc607f213c86ece74da&units=imperial")
+	  .then(response => {
+		  return response.json();
+		  })
+	  .then (responseData => {
+		  console.log(responseData);
+		  weather = responseData.weather[0].description;
+		  temp = Math.floor(responseData.main.temp);
+		  document.getElementById("tempF").innerHTML = temp +" &#176F";
+		  bkgWeather();
+	  })
+	  .catch(err => {
+		  console.error(err);
+	  });
+  }
+
+  window.addEventListener("load", getLocation);
+   							// WEATHER API END
+const body = document.getElementById("html")
+function bkgWeather() {
+switch(weather) {
+	case "broken clouds": body.style.backgroundImage = "url(./resources/img/some-clouds.jpg)"
+	break;
+	case "moderate rain": body.style.backgroundImage = "url(./resources/img/moderate-rain.jpg)"
+	break;
+	case "light rain": body.style.backgroundImage = "url(./resources/img/light-rain.jpg)"
+	break;
+	case "heavy rain": body.style.backgroundImage = "url(./resources/img/heavy-rain.jpg)"
+	break;
+	case "scattered clouds": body.style.backgroundImage = "url(./resources/img/some-clouds.jpg)"
+	break;
+	case "overcast clouds": body.style.backgroundImage = "url(./resources/img/heavy-clouds.jpg)"
+	break;
+	case "light snow": body.style.backgroundImage = "url(./resources/img/light-snow.jpg)"
+	break;
+	case "heavy snow": body.style.backgroundImage = "url(./resources/img/heavy-snow.jpg)"
+	break;
+	case "clear sky": body.style.backgroundImage = "url(./resources/img/clear.jpg)"
+	break;
+
+		}
+	
+}
